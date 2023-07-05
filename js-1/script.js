26a6f15c4b1e1266347ba000bd5faca37db88756
danyos <daniel.hambardzumyan@mail.ru>
2023-07-05 20:33:18 +0400

    javascript first lesson

---
+++ js-1/script.js
@@ -0,0 +1,61 @@
+// Javascript main type
+
+// let  a=70
+// let b=25
+// let c=0
+// c=a//7
+// a=b//5
+// b=c//7
+//
+//
+// console.log(a)//5
+// console.log(b)//7
+
+// let x=5
+// let y=7
+// console.log(x)
+// console.log(y)
+
+// alert(1)
+// alert(2)
+// alert(3)
+
+const myBirthday = '18.04.1982';
+ // myBirthday = '17';
+
+
+
+
+
+
+
+// Number
+// 1+1
+// 2*3
+// 2-3
+// 8 / 4
+// 2 ** 5 //2*2*2*2*2
+// 270%70
+// 70->140->210
+// 1,2,3,4,5,6,7,8,9
+
+// console.log(2 ** 5)
+
+
+
+
+
+// String
+
+//"artahatyutyuneri het ashxatel"
+
+// Boolean
+
+// true false
+
+// Null
+
+// undefined
+
+// Object
+
