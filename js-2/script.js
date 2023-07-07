575595c2fab5e4f61db316b59a4409bb4a7d8cd5
danyos <daniel.hambardzumyan@mail.ru>
2023-07-07 20:55:06 +0400

    Basic operators, maths

---
+++ js-2/script.js
@@ -0,0 +1,56 @@
+// let x=7
+// let y=5
+// x+=y // 12  // x=x+y
+// //y-=x// -7
+// y=x-y
+// x-=y//5
+//
+//
+// // console.log("barev")
+// // alert(1)
+// // alert(2)
+// // alert(3)
+// // alert(4)
+// // console.log("hajox")
+//
+// let hello = 'Hello world!';
+//
+// let message;
+//
+// message = hello; //'Hello world!';
+//
+// hello="Barev bolorin"
+//
+//
+//
+// console.log(message)
+// console.log(hello)
+//
+// t=5
+// console.log(t)
+
+
+let refresh='8'
+let succses=2
+// refresh=succses  // nor veragrum refreshin succsesi arjeqy
+console.log(typeof (refresh - succses))
+
+
+
+let t=7
+
+let worning="1.5$ " + t
+let y=`dvzd fvzd ${t} gvdfvb`
+
+
+
+
+
+
+//
+// // succses+=1
+// // succses=succses+1
+// succses++
+// ++succses
+// console.log(succses)
+// console.log(succses)
