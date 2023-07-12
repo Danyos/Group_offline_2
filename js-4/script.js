090a1b004af16087672bdfaf785cc8af651d45a5
danyos <daniel.hambardzumyan@mail.ru>
2023-07-12 20:40:02 +0400

    string methods

---
+++ js-4/script.js
@@ -0,0 +1,53 @@
+// JavaScript false -i arjeqnery
+
+// console.log(Boolean(undefined))
+// let x=prompt("grew tiv")
+//
+// // console.log((x>=18 && x<=63)? "chisht e":"sxal e")
+// console.log(  (x>=18 && x<=63) && confirm("Duq txamard es"))
+
+// let x=confirm("Who's there?")
+// let y= x&&prompt("") || null
+// console.log(y)
+
+
+// undefined==null
+// true
+// !!undefined===!!!null
+// false
+// !!undefined===!!null
+// true
+// !!""
+// false
+// !!""
+// false
+// "".length
+// 0
+// !!0
+// false
+
+
+//
+// let num=1458
+// num=""+num
+// console.log(num.length%2 === 0)
+
+
+// let str="barev Anuns 233 Daniel e,  Yes  tarekan em Bnakvum em Ejmaidzinum, Anuns drel en Daniel Margarei patvin"
+// str=str.replaceAll('Daniel',"Hmayak")
+
+// let y=str.length-1
+// console.log(str[y])
+// str=str.slice(0,11) + " Hamayak " + str.slice(-1)
+// console.log(str.slice(-9,-5))
+
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str.indexOf(23))
+// console.log(str.slice(str.indexOf(23),str.indexOf(23)+3))
+
+
+
+//
+// console.log((0 || 1 || false)&&8)
