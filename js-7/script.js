2f0f1feccf4c051d77161f158889c0c195b2dc7b
danyos <daniel.hambardzumyan@mail.ru>
2023-07-19 20:59:10 +0400

    test string

---
+++ js-7/script.js
@@ -0,0 +1,53 @@
+// // console.log(1<5&&2*8)
+// const login='Admin123'
+// const password="A123456"
+//
+// let res=prompt('Greq',"Admin123").toLowerCase()===login.toLowerCase()
+//     && password===prompt("greq password")
+// console.log(res)
+
+// tnayin
+
+// 1
+let str='Lorem ipsum dolor. sit amet consectetur adipisicing elit. Ab earum sit amet consectetur sit amet consectetur adipisicing elit. elit.'
+
+// ktrel 1 in ketic minchev 2 kety bayc dinamic devov
+
+// 2.
+let num="96,84"
+let x=9
+let y=4
+// ktrel tivyx i mej pahel minchev ,@ y i mej pahel , ic heto
+// dyunamics devov
+
+
+// 3. stexcum enq mutqi sxemaa
+const login="admin123"
+const psw='aA1234'
+// ____________________________
+
+// let log=prompt("greq login")
+// let password=prompt("greq psw")
+
+// 1. Hamematel login ardyoq hamapataxanum e login te voch
+// 2. loginy kap chuni ogtatery metsatar e grel te poqratar
+// 3. erb loginy chisht lini nor stugi psw
+
+
+// tnayin
+
+//1. promptov mardy gri anun cankacac paragayum arajin tary sarqi poqratar miayn arajin tary
+// HAMAYAK>hAMAYAK
+// 2. nuyni verjin tary metsatar
+// Muxi>> MuxI
+
+// 3.prompt unaenaq stugum eq, թե արդյոք թիվը համապատասխանում է հետևյալ չափանիշներին.
+// Թիվը դրական է։
+// Թիվը զույգ է։
+// Թիվը բաժանվում է 3-ի
+
+// promptum gres faringeyti tivy veradarcni C astichan taki orinaky matematikakan grvacq e
+// (°F - 32) * 5/9 = °C
+//
+//// promptum gres celsus tivy veradarcni F astichan taki orinaky matematikakan grvacq e
+// (°C * 9/5) + 32 = °F
