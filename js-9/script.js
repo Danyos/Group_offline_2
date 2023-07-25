20162bed0274e9652eb0622623287f9ad611028a
danyos <daniel.hambardzumyan@mail.ru>
2023-07-25 22:51:57 +0400

    for

---
+++ js-9/script.js
@@ -0,0 +1,55 @@
+// let x=8
+// let y=7
+// let z="+"
+// let res=0
+// switch (z){
+//     case "+":
+//         res=x+y
+//         console.log("yes 10 rd andamn em")
+//         break
+//     case "-":
+//         res=x-y
+//         console.log("yes 20 rd andamn em")
+//         break
+//     case "*":
+//         res=x*y
+//         console.log("yes 50 rd andamn em")
+//         break
+//     case "/":
+//         res=x/y
+//         console.log("yes 78 rd andamn em")
+//         break
+//     case "**":
+//         console.log("yes 80 rd andamn em")
+//         res=x**y
+//         break
+//     default:
+//         console.log("yesel elsen em")
+//
+// }
+// console.log(res)
+//
+// console.time("p1")
+//
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+//
+// }
+// console.timeEnd("p1")
+
+
+
+
+// 1. 1-100
+// gtnel ayn tvery voronq bajanvum en 2 i ev 3 i
+// 2. tpel miayn kentery kam zuygery
+// 3. 3 i vra e bajanvum tpi fiz ete 5 vra biz ete ham 3 i ham 5 i tpi fizbiz
+// 4. promptov greq tiv veadarcni minchev ayd tvi gumar
+// 5. unenal mek prompt vory kgrvi ayd devachapov (2,5) inqy veadarcni 2*2*2*2*2//2**5
+
+// console.log(2**5)
+
+
+
+
+
